9492545b55f4d6f09ca1862dd8f1897e61787380
Tarek <83262890+tarek102@users.noreply.github.com>
2022-06-30 02:43:18 +0200

    Update styles.css Create mobile menu wrapper

---
+++ script.js
@@ -0,0 +1,18 @@
+
+/* Variables */
+
+let hamburgerBtn = document.querySelector('.hamburger-menu');
+let heroSec = document.querySelector('.hero-sec');
+
+
+/* function */
+
+function openMenu() {
+  
+}
+
+
+
+/* Events */
+
+hamburgerBtn.addEventListener('click', openMenu)
