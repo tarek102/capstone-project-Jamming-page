ad655cc034d2c6138d73706b1239c6ab87106f86
Tarek <83262890+tarek102@users.noreply.github.com>
2022-06-30 13:39:50 +0200

    fix linters errors

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-
 /* Variables */
 
-let hamburgerBtn = document.querySelectorAll('.hamburger-menu');
-let heroSec = document.querySelector('.hero-sec');
-let aboutSec = document.querySelector('.about-sec');
-let menuWrapper = document.querySelector('.menu-wrapper');
-let closeBtn = document.querySelector('.menu-wrapper img');
-let speakersBoxes = document.querySelector('.speakers-boxes');
-let players = [
+const hamburgerBtn = document.querySelectorAll('.hamburger-menu');
+const menuWrapper = document.querySelector('.menu-wrapper');
+const closeBtn = document.querySelector('.menu-wrapper img');
+const speakersBoxes = document.querySelector('.speakers-boxes');
+const players = [
   {
-    playerName  : "David Gilmour",
-    title   : "Lead Guitarist",
-    description : "Join our community now and play the guitar along with David Gilmour",
-    imagePath : "./images/Untitled design (1).png"
+    playerName: 'David Gilmour',
+    title: 'Lead Guitarist',
+    description: 'Join our community now and play the guitar along with David Gilmour',
+    imagePath: './images/Untitled design (1).png',
   },
   {
-    playerName  : "Mark Lettieri",
-    title   : "Lead Guitarist",
-    description : "Mark is the lead guitarist for Snarky Puppy, and he is willing to teach you more about Music theory",
-    imagePath : "./images/Untitled design (6).png"
+    playerName: 'Mark Lettieri',
+    title: 'Lead Guitarist',
+    description: 'Mark is the lead guitarist for Snarky Puppy, and he is willing to teach you more about Music theory',
+    imagePath: './images/Untitled design (6).png',
   },
   {
-    playerName  : "Eric Clapton",
-    title   : "Lead Guitarist",
-    description : "Join us now and listen to the best tunes from the best guitarists of all time",
-    imagePath : "./images/Untitled design (7).png"
+    playerName: 'Eric Clapton',
+    title: 'Lead Guitarist',
+    description: 'Join us now and listen to the best tunes from the best guitarists of all time',
+    imagePath: './images/Untitled design (7).png',
   },
   {
-    playerName  : "Chris Wolstenholme",
-    title   : "Bassist",
-    description : "Chris will join our gathering and play the best rifs along side with you",
-    imagePath : "./images/Untitled design (8).png"
-  }
-]
-
+    playerName: 'Chris Wolstenholme',
+    title: 'Bassist',
+    description: 'Chris will join our gathering and play the best rifs along side with you',
+    imagePath: './images/Untitled design (8).png',
+  },
+];
 
 /* function */
 
 function openMenu() {
-  menuWrapper.classList.add('menu-wrapper-active')
-  alert(8)
+  menuWrapper.classList.add('menu-wrapper-active');
 }
 
 function closeMenu() {
-  menuWrapper.classList.remove('menu-wrapper-active')
+  menuWrapper.classList.remove('menu-wrapper-active');
 }
 
 players.forEach((player) => {
-  let speakerInfo = document.createElement('div');
+  const speakerInfo = document.createElement('div');
   speakerInfo.classList.add('speaker-info');
   speakerInfo.innerHTML = `
   <div class="speaker-left">
@@ -62,13 +57,11 @@ players.forEach((player) => {
   </div>`;
 
   speakersBoxes.appendChild(speakerInfo);
-})
+});
 
 /* Events */
 
-
 hamburgerBtn.forEach((btn) => {
   btn.addEventListener('click', openMenu);
-
-})
+});
 closeBtn.addEventListener('click', closeMenu);
